ae196dfdf78a077cedcad06afa129f60ab5e63d3
Nakayama Reiya <nkymriy@gmail.com>
2019-10-08 00:15:03 +0900

    [UPDATE]DynamoDBの接続(成功)

--- armageddon_system/views.py
+++ armageddon_system/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from armageddon_system.dynamo_models import dynamodb_model
 
 def index(request):
-    db = dynamodb_model.dynamo.get("armageddon")
+    db = dynamodb_model.dynamo.get(hash_key="armageddon",range_key="1")
 
 
-    return render(request, 'armageddon_system/index.html',{'msg':db})
+    return render(request, 'armageddon_system/index.html',{'msg':db.attribute_values})
