fa274c4af62d05a60f42b1d562f07a8d9a818970
NakayamaReiya <1901029@s.asojuku.ac.jp>
2019-10-08 14:32:36 +0900

    [TEST]DynamoDBから情報を取得して表示するテスト

--- armageddon_system/views.py
+++ armageddon_system/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from armageddon_system.dynamo_models import dynamodb_model
+from armageddon_system.models import dynamodb_model
 
 def index(request):
-    db = dynamodb_model.dynamo.get("armageddon")
-
-
-    return render(request, 'armageddon_system/index.html',{'msg':db})
+    db = dynamodb_model.dynamo.get(hash_key="armageddon",range_key=1)
+    db_values = db.attribute_values
+    return render(request, 'armageddon_system/index.html',{'msg':db_values.get("Forms")[0]})
