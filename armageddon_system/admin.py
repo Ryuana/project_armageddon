38c250f1bcf9ca6c7a1f530f1ee5bf076091b4fb
NakayamaReiya <1901029@s.asojuku.ac.jp>
2019-10-08 11:25:36 +0900

    [UPDATE]Dynamoのキーを隠蔽
    envファイルをarmageddon_system直下に設置。(.gitignoreに追加)

--- armageddon_system/admin.py
+++ armageddon_system/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
 
-# Register your dynamo_models here.
+# Register your models here.
--- armageddon_system/models/dynamodb_model.py
+++ armageddon_system/models/dynamodb_model.py
@@ -1,13 +1,13 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
-
+import armageddon_system.env as env
 
 class dynamo(Model):
     class Meta:
-        aws_access_key_id = 'AKIA2SY5EY222UH55GWG'
-        aws_secret_access_key = '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'
-        table_name = "armageddon_db"
-        region = 'ap-northeast-1' #東京リージョン
+        aws_access_key_id = env.AWS_ACCESS_KEY_ID
+        aws_secret_access_key = env.AWS_SECRET_ACCESS_KEY
+        table_name = env.AWS_TABLE_NAME
+        region = env.AWS_REGION
 
     AppName = UnicodeAttribute(hash_key=True)
     AppNo = UnicodeAttribute(range_key=True)
