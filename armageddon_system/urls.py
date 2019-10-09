7466d150214e5d67d3e358151fe21bf211ccef3e
ASO1701016 <1701016@st.asojuku.ac.jp>
2019-10-09 15:01:05 +0900

    [TEST]commit確認（feature/view）

--- armageddon_system/urls.py
+++ armageddon_system/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-import armageddon_system.views as views
+from armageddon_system.views.user import login, logout
+from armageddon_system.views.register import confirm,form
+from armageddon_system.views.linebot.qa import list
+from armageddon_system.views.linebot.msg import list, edit
+from armageddon_system.views.menu import item
+from armageddon_system.views.pay import log
+from armageddon_system.views.pay.item import list,qr
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('user/login', login, name='login'),
 ]
