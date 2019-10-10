0c000945bda0bf6236fbdec47379a7719c3269dd
NakayamaReiya <1901029@s.asojuku.ac.jp>
2019-10-10 11:40:06 +0900

    [ADD]viewsフォルダに__init__.pyを設置
    こうすることでimportと関数の使用が簡略化できる

--- armageddon_system/urls.py
+++ armageddon_system/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from armageddon_system.views.user import login, logout
-from armageddon_system.views.register import confirm,form
-from armageddon_system.views.linebot.qa import list
-from armageddon_system.views.linebot.msg import list, edit
-from armageddon_system.views.menu import item
-from armageddon_system.views.pay import log
-from armageddon_system.views.pay.item import list,qr
+from . import views
 
 urlpatterns = [
-    path('user/login', login, name='login'),
+    path('user/login', views.login, name='login'),
 ]
