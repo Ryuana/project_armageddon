4a7cc4e4fc6ca218c03f1410c34e4c625f718ec7
Ryu <1801179@s.asojuku.ac.jp>
2019-10-27 00:29:37 +0900

    [ADD]messageクラスを追加

---
+++ armageddon_system/models/message.py
@@ -0,0 +1,12 @@
+class Message:
+
+    message_id = 0
+    message = ""
+    image:bytearray = ""
+    create_date = ""
+
+    def __init__(self, message_id, message, image, create_date):
+        self.message_id = message_id
+        self.message = message
+        self.image = image
+        self.create_date = create_date
