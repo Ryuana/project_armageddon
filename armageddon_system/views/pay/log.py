27930c523c7548877b23de221b2f018fc7a4dda7
Ryu <1801179@s.asojuku.ac.jp>
2019-10-27 21:36:23 +0900

    [FIX]log.pyファイルの渡す値を追加

--- armageddon_system/views/pay/log.py
+++ armageddon_system/views/pay/log.py
@@ -7,9 +7,9 @@ def display_pay_logs(request):
 
     pay_logs: list[PayLog] = db.get_pay_log_all()
 
-    return render(request, 'armageddon_system/pay/log.html')
+    return render(request, 'armageddon_system/pay/log.html', {'pay_logs': pay_logs})
 
-def export_pay_logs(request,pay_logs:PayLog):
+def export_pay_logs(request, pay_logs: PayLog):
 
     """
     CSV出力処理
