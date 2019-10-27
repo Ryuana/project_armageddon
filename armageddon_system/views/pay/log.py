583411d899ae316d9a057275f279ea6e50f83d88
Ryu <1801179@s.asojuku.ac.jp>
2019-10-27 21:28:12 +0900

    [ADD]log.pyファイルを追加

---
+++ armageddon_system/views/pay/log.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from armageddon_system.models.DynamoManager import DynamoManager as db
+from armageddon_system.models.pay_log import PayLog
+from django.http import HttpResponse
+
+def display_pay_logs(request):
+
+    pay_logs: list[PayLog] = db.get_pay_log_all()
+
+    return render(request, 'armageddon_system/pay/log.html')
+
+def export_pay_logs(request,pay_logs:PayLog):
+
+    """
+    CSV出力処理
+    """
+
+    return HttpResponse()
