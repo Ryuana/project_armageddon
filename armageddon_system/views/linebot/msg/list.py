1e8098a7c2d8fb0ceb248c19bd5f2fa4e643cc2e
Ryu <1801179@s.asojuku.ac.jp>
2019-10-27 23:47:52 +0900

    [FIX]qa/list.pyファイルの関数名を修正

--- armageddon_system/views/linebot/msg/list.py
+++ armageddon_system/views/linebot/msg/list.py
@@ -3,13 +3,13 @@ from armageddon_system.models.message import Message
 from armageddon_system.models.DynamoManager import DynamoManager as db
 from django.http import HttpResponse
 
-def display_form_item(request):
+def display_message_item(request):
 
     messages: list[Message] = db.get_message_list
 
     return render(request, 'armageddon_system/pay/item/list.html', {'messages': messages})
 
-def delete_form_item(request, message_id):
+def delete_message_item(request, message_id):
 
     """
     message_idをもとに該当するデータを削除する処理
