17c15d19a4aacf33f0d5c848ea27efda43ea0704
NakayamaReiya <1901029@s.asojuku.ac.jp>
2019-11-12 14:34:56 +0900

    [UPDATE]不要なmenuのviewを削除

--- armageddon_system/urls.py
+++ armageddon_system/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('pay/item/list', views.pay.item_list, name='item_list'),
     path('pay/item/qr', views.pay.item_qr, name='item_qr'),
     path('pay/log', views.pay.log, name='pay_log'),
-    path('menu/item', views.menu.menu_item, name='menu_item'),
     path('linebot/qa/list', views.linebot.qa_list, name='qa_list'),
     path('linebot/msg/edit', views.linebot.msg_edit, name='msg_edit'),
     path('linebot/msg/list', views.linebot.msg_list, name='msg_list'),
--- armageddon_system/views/menu.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def menu_item(request):
-    return render(request, 'armageddon_system/menu/item.html')
-
-def logout(request):
-    request.session.clear()
-    return render(request, 'armageddon_system/user/login.html')
