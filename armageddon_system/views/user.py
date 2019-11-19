c08550812f43e5c2d9d6dd29fa7059295d6e6646
Ryu <1801179@s.asojuku.ac.jp>
2019-11-19 10:27:24 +0900

    [UPDATE]viewのuserクラス完成

--- armageddon_system/views/user.py
+++ armageddon_system/views/user.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render
 from armageddon_system.views import pay
+from armageddon_system.models.dynamo_manager import DynamoManager as db
 
 def login(request):
 
     if request.method == 'GET':
         return render(request, 'armageddon_system/user/login.html')
+
     elif request.method == 'POST':
         is_login_success = False
+        dbm = db()
 
-        if 'ID' in request.POST and 'pass' in request.POST:
-            request.session['user_id'] = request.POST['ID']
-            request.session['user_pass'] = request.POST['pass']
-            is_login_success = True
-            #session_saveメソッドを作る
+        if 'user_id' in request.POST and 'user_pass' in request.POST:
+            user_id = request.POST['user_id']
+            user_pass = request.POST['user_pass']
+
+            if dbm.check_login_id(user_id, user_pass):
+                request.session['user_id'] = user_id
+                request.session['user_pass'] = user_pass
+                is_login_success = True
+                #session_saveメソッドを作る
+
+            else:
+                context = {}
+                context['error'] = "IDまたはパスワードが違います"
 
         if is_login_success:
             return pay.log(request)
-            # return render(request, 'armageddon_system/pay/log.html')
+
         else:
-            return render(request, 'armageddon_system/user/login.html')
+            return render(request, 'armageddon_system/user/login.html', context)
 
 def logout(request):
 
     request.session.clear()
-
     return render(request, 'armageddon_system/user/login.html')
