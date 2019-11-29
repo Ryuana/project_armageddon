e6efa20ed8890907fc1cd570e14559b94d030bd2
NakayamaReiya <1901029@s.asojuku.ac.jp>
2019-11-29 12:03:16 +0900

    [FIX]カンマが抜けていた不具合を修正

--- project_armageddon/settings.py
+++ project_armageddon/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     "ec2-13-113-87-53.ap-northeast-1.compute.amazonaws.com",
     'localhost',
-    '192.168.43.204' #テザリング用
+    '192.168.43.204', #テザリング用
     '13.113.87.53'
 ]
 
