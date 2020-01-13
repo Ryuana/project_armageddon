4008286a9212ee50be3fc5fa5ddfc39b7dc8d8df
Nakayama Reiya <nkymriy@gmail.com>
2020-01-14 00:27:28 +0900

    [FIX]importからDebugを削除

--- armageddon_system/views/__init__.py
+++ armageddon_system/views/__init__.py
@@ -1,5 +1,4 @@
 from armageddon_system.views import linebot
 from armageddon_system.views import pay
 from armageddon_system.views import register
-from armageddon_system.views import debug
 from armageddon_system.views import user
